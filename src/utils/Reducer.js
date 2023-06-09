c020e5d5dc210e62ee175f3c6fddfdfadedb2ca5
Benson Ndiwa <bnkimtai@gmail.com>
2023-06-09 20:24:50 +0300

    Reducer

---
+++ src/utils/Reducer.js
@@ -0,0 +1,45 @@
+export const initialState = {
+    user: '',
+    activities: []
+}
+
+// {
+//     title: null,
+//     description: null,
+//     date: '',
+//     startTime:'',
+//     endTime: '',
+//     repeatEvery:{
+//         sun:false,
+//         mon: false,
+//         tue: false,
+//         wed: false,
+//         thur: false,
+//         fri: false,
+//         sat: false
+//     },
+//     notifyBefore:''
+// }
+
+const Reducer = (action , state) =>{
+    const { type, payload } = action;
+    switch(type){
+        case "ADD_ACTIVITY":
+            let addState ={
+                ...state,
+                activities: payload.activites
+            }
+            return addState;
+        case "DELETE_ACTIVITY":
+            let newState = {
+                ...state,
+                activities: payload.activities
+            }
+            return newState;
+        default:
+            throw new Error("No case found in reducer");
+
+    }
+}
+
+export default Reducer;
